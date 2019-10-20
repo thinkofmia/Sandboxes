f5f8189919b9f17d2f27041718cd63643cf3035d
FremontTeng <fremontteng@gmail.com>
2019-10-20 18:37:09 +0800

    Adding sandbox

---
+++ julia-sandbox.jl
@@ -0,0 +1,9 @@
+using Printf
+using Statistics
+# Importing modules
+
+#Variables can be used by numbers, underscores, exclaimations or alphabets
+s = 0
+#Variables are dynamically assigned
+s = 'Dog'
+println(s)
