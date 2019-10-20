5482ae2fd3819625b903d91cae185518bf183d33
FremontTeng <fremontteng@gmail.com>
2019-10-20 19:01:32 +0800

    Added functions

--- julia-sandbox.jl
+++ julia-sandbox.jl
@@ -7,3 +7,17 @@ s = 090
 #Variables are dynamically assigned
 s = "Cat"
 println(s)
+
+#Assert Data Types
+function changeTypes()
+    x::Int8 = 100
+    #x = "Meow"
+end
+
+println(changeTypes())
+
+#booleans either true or false
+hasFur = true 
+
+#Int8: -128, 127
+#Int16: -32 768, 32 767
