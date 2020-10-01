9d9ae0243937abfae341c80c595856aa851dea43
FremontTeng <fremontteng@gmail.com>
2020-10-01 17:36:18 +0800

    Added events and mouseover canvase

--- Vue-Js-2/app.js
+++ Vue-Js-2/app.js
@@ -4,14 +4,33 @@ new Vue({
     data: {
         name: 'Nina',
         job: 'Breaker',
+        age: '22',
+        x: '0',
+        y: '0',
         website: 'http://www.thenetninja.co.uk',
         websiteTag: '<a href="http://www.thenetninja.co.uk">The Net Ninja Website</a>'
     },
     //Method is a function
     methods: {
+        //Function to greet user by their name
         greet: function(time){
             //this.data.name;//Access all the data in this function
             return 'Good '+ time + " " + this.name;
+        },
+        //Functions to add/subtract to age
+        add: function(inc){
+            this.age += inc
+        },
+        subtract: function(dec){
+            this.age -= dec
+        },
+        //Function to update x y coordinates
+        updateXY: function(event){
+            //Debug message to see event parameters
+            //console.log(event);
+            //Set new coordinates
+            this.x = event.offsetX;
+            this.y = event.offsetY; 
         }
     }
 });
