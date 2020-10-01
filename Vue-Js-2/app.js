c21774c1197c337810b15113cc409616a0b3daaf
FremontTeng <fremontteng@gmail.com>
2020-10-01 17:08:37 +0800

    Add methods

--- Vue-Js-2/app.js
+++ Vue-Js-2/app.js
@@ -2,6 +2,14 @@ new Vue({
     //El proxy - Vue js gonna make an instance with html element #vue-app
     el:'#vue-app',
     data: {
-        name: 'Nina'
+        name: 'Nina',
+        job: 'Breaker'
+    },
+    //Method is a function
+    methods: {
+        greet: function(time){
+            //this.data.name;//Access all the data in this function
+            return 'Good '+ time + " " + this.name;
+        }
     }
 });
