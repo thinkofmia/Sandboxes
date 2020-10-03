47c67607a8515067d48ca5da68a0111df785a44e
FremontTeng <fremontteng@gmail.com>
2020-10-03 11:47:48 +0800

    Added components.

--- Vue-Js-2/one_app.js
+++ Vue-Js-2/one_app.js
@@ -1,3 +1,25 @@
+data = {
+    name: 'Hitomi'
+}
+
+//Components
+//Resuable piece of code/template to use in different instances
+Vue.component('greeting',{
+    //Change only target component, not across all
+    template: '<p>Hey there, I am {{name}}. <button v-on:click="changeName">Change Name</button></p>',
+    data: function(){
+        //return data //Tricks the data to change all instead of one
+        return {
+            name: 'Hitomi'
+        }
+    },
+    methods: {
+        changeName: function(){
+            this.name = 'Helena';
+        }
+    }
+});
+
 var one = new Vue({
     el: '#vue-app-one',
     data:{
