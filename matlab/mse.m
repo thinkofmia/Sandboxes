fa3edcf10fd47edb47e1e331130e5b0722b724ad
thinkofmia <thinkofmia@gmail.com>
2021-04-25 11:48:08 +0800

    Added matlab code

---
+++ matlab/mse.m
@@ -0,0 +1,6 @@
+img = imread('cameraman.tif');
+subimg1 = img(1:50,1:50);
+subimg2 = img(  end -49 :end,  end -49 :end);
+SSD = sum(sum((double(subimg1) - double(subimg2)).^2));
+SSD = immse(subimg1, subimg2) * numel(subimg1);
+disp(SSD)
