1f41439b7245be1a0af1844c95ee54cfd308e386
thinkofmia <thinkofmia@gmail.com>
2021-06-11 13:10:56 +0800

    Added colors

--- React-Native/helloworld.js
+++ React-Native/helloworld.js
@@ -4,7 +4,7 @@ import { Text, View, StyleSheet } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}> Hello World </Text>
+      <Text style={styles.text}> Hello World! </Text>
     </View>
   );
 }
