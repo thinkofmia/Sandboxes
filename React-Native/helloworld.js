8cd37cba6a9368b1ab5030c70e07bae7d6f7597e
thinkofmia <thinkofmia@gmail.com>
2021-06-11 13:03:40 +0800

    Added hello world base file

---
+++ React-Native/helloworld.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text> Go to App.js to start editing your app. </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
