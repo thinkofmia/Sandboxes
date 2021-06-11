757512c2345258ebe5a71e9362c127dad27102eb
thinkofmia <thinkofmia@gmail.com>
2021-06-11 17:26:18 +0800

    Added namecard

--- React-Native/helloworld.js
+++ React-Native/helloworld.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 
 export default function App() {
   return (
@@ -7,6 +7,14 @@ export default function App() {
       <View style={styles.fillerContainer}></View>
       <View style={styles.titleContainer}>
         <Text style={styles.text}> Hello World! </Text>
+        <Text style={styles.subtitle}>Introducing... Charmander</Text>
+        <Image
+          style={styles.profilePic}
+          source={{
+            uri:
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWy1rhx6EXXOp-o6Jh89UM-FYySA8Ic34LFQ&usqp=CAU',
+          }}
+        />
       </View>      
       <View style={styles.fillerContainer}></View>
     </View>
@@ -21,7 +29,7 @@ const styles = StyleSheet.create({
     backgroundColor: "goldenrod"
   },
   text : {
-    fontSize: 60,
+    fontSize: 50,
     fontWeight: "bold",
     color: "white",
     textAlign: "center",
@@ -39,5 +47,19 @@ const styles = StyleSheet.create({
     flex: 0.5,
     justifyContent: "center",
     borderRadius: "25px",
+  },
+  subtitle: {
+    color: 'red',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  profilePic: {
+    height: 120,
+    width: 120,
+    borderRadius: 60,
+    alignSelf: 'center',
+    marginTop: 10,
+    borderColor: 'white',
+    borderWidth: 2,
   },
 });
