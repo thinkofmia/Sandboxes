a5c9a318d1f6a38eee96d44c9986819626cee11d
thinkofmia <thinkofmia@gmail.com>
2021-06-12 18:14:59 +0800

    Add display on count

--- React-Native/clickerapp.js
+++ React-Native/clickerapp.js
@@ -12,7 +12,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{count}</Text>
       <Button title='Press Me!' onPress={buttonPressed} ></Button>
     </View>
   );
