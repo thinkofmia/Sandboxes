67d276f9a64a3c5ae379be333ac5682fc56d76b6
thinkofmia <thinkofmia@gmail.com>
2021-07-10 23:06:44 +0800

    Remove old phaser file. Added preload method

--- Phaser3/ZenvaTutorial/js/game.js
+++ Phaser3/ZenvaTutorial/js/game.js
@@ -1,6 +1,12 @@
 // create a new scene
 let gameScene = new Phaser.Scene('Game');
 
+//Load Assets
+gameScene.preload = function(){
+  //Load Images
+  this.load.image('background','assets/background.png')
+}
+
 // set the configuration of the game
 let config = {
   type: Phaser.AUTO, // Phaser will use WebGL if available, if not it will use Canvas
