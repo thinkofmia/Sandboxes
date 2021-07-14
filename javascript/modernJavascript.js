26f8382a35b8882e6f7510009508d0213014e3b6
thinkofmia <thinkofmia@gmail.com>
2021-07-14 23:52:52 +0800

    Add random js stuffs

--- javascript/modernJavascript.js
+++ javascript/modernJavascript.js
@@ -1,1 +1,15 @@
 console.log('Hello world!');
+
+var age = 26;
+age = 27;
+age = age + 1;
+
+var health = 0.65;
+
+var isAlive = true;
+isAlive = false;
+
+isAlive = 5 > 2;
+
+const pi = 3.14159;
+let numberOfLimbs = 4;
