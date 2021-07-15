57734c93b0047b235d6eaac3009f88e13db5b559
thinkofmia <thinkofmia@gmail.com>
2021-07-15 11:17:43 +0800

    Create get request and route

--- javascript/rest_api/index.js
+++ javascript/rest_api/index.js
@@ -2,6 +2,11 @@ const express = require ('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (request, response)=>{
+    console.log(request);
+    response.send('Hello world');
+});
+
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
 });
