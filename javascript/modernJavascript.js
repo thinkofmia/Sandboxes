11410a7bce9272fc6a25e8678d6ad2f1244af989
thinkofmia <thinkofmia@gmail.com>
2021-07-15 08:40:21 +0800

    Fetching data from url

--- javascript/modernJavascript.js
+++ javascript/modernJavascript.js
@@ -66,3 +66,17 @@ async function sum(number1, number2 = 2) {
 };
 
 sum(1,2);
+
+const fetch = require('node-fetch');
+
+let url = 'http://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/?gameid=221380&format=xml';
+
+async function fetchData(url){
+    let response = await fetch(url);
+    let jsonResponse = await response.json();
+    console.log(JSON.stringify(jsonResponse));
+}
+
+fetchData(url).catch(function(){
+    console.log('Could not fetch Data');
+});
