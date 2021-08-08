13532d9ac1eb0e3c678216d9b6abba9d032dff95
thinkofmia <thinkofmia@gmail.com>
2021-08-08 17:08:06 +0800

    Add default file for mongodb and express

---
+++ MongoDB + Express/routes/main.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const tokenList = {};
+const router = express.Router();
+
+router.get('/', (request, response) => {
+  response.send('Hello world');
+});
+
+router.get('/status', (request, response) => {
+  response.status(200).json({ message: 'ok', status: 200 });
+});
+
+router.post('/signup', passport.authenticate('signup', { session: false }), async (request, response, next) => {
+  response.status(200).json({ message: 'signup successful', status: 200 });
+});
+
+router.post('/login', async (request, response, next) => {
+  passport.authenticate('login', async (error, user) => {
+    try {
+      if (error) {
+        return next(error);
+      }
+      if (!user) {
+        return next(new Error('email and password are required'));
+      }
+
+      request.login(user, { session: false }, (err) => {
+        if (err) return next(err);
+
+        // create our jwt
+        const body = {
+          _id: user._id,
+          email: user.email,
+          name: user.username,
+        };
+
+        const token = jwt.sign({ user: body }, process.env.JWT_SECRET, { expiresIn: 300 });
+        const refreshToken = jwt.sign({ user: body }, process.env.JWT_REFRESH_SECRET, { expiresIn: 86400 });
+
+        // store tokens in cookie
+        response.cookie('jwt', token);
+        response.cookie('refreshJwt', refreshToken);
+
+        // store tokens in memory
+        tokenList[refreshToken] = {
+          token,
+          refreshToken,
+          email: user.email,
+          _id: user._id,
+          name: user.name,
+        };
+
+        // send the token to the user
+        return response.status(200).json({ token, refreshToken, status: 200 });
+      });
+    } catch (err) {
+      console.log(err);
+      return next(err);
+    }
+  })(request, response, next);
+});
+
+router.post('/logout', (request, response) => {
+  if (request.cookies) {
+    const refreshToken = request.cookies.refreshJwt;
+    if (refreshToken in tokenList) delete tokenList[refreshToken];
+    response.clearCookie('jwt');
+    response.clearCookie('refreshJwt');
+  }
+  response.status(200).json({ message: 'logged out', status: 200 });
+});
+
+router.post('/token', (request, response) => {
+  const { refreshToken } = request.body;
+  if (refreshToken in tokenList) {
+    const body = {
+      email: tokenList[refreshToken].email,
+      _id: tokenList[refreshToken]._id,
+      name: tokenList[refreshToken].name,
+    };
+    const token = jwt.sign({ user: body }, process.env.JWT_SECRET, { expiresIn: 300 });
+
+    // update jwt
+    response.cookie('jwt', token);
+    tokenList[refreshToken].token = token;
+
+    response.status(200).json({ token, status: 200 });
+  } else {
+    response.status(401).json({ message: 'unauthorized', status: 401 });
+  }
+});
+
+module.exports = router;
---
+++ MongoDB + Express/routes/password.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const hbs = require('nodemailer-express-handlebars');
+const nodemailer = require('nodemailer');
+const path = require('path');
+const crypto = require('crypto');
+
+const UserModel = require('../models/UserModel');
+
+const email = process.env.EMAIL;
+const password = process.env.PASSWORD;
+
+const smtpTransport = nodemailer.createTransport({
+  service: process.env.EMAIL_PROVIDER,
+  auth: {
+    user: email,
+    pass: password,
+  },
+});
+
+const handlebarsOptions = {
+  viewEngine: {
+    extName: '.hbs',
+    defaultLayout: null,
+    partialsDir: './templates/',
+    layoutsDir: './templates/'
+  },
+  viewPath: path.resolve('./templates/'),
+  extName: '.html',
+};
+
+smtpTransport.use('compile', hbs(handlebarsOptions));
+
+const router = express.Router();
+
+router.post('/forgot-password', async (request, response) => {
+  const userEmail = request.body.email;
+  const user = await UserModel.findOne({ email: userEmail });
+  if (!user) {
+    response.status(400).json({ message: 'invalid email', status: 400 });
+    return;
+  }
+
+  // create user token
+  const buffer = crypto.randomBytes(20);
+  const token = buffer.toString('hex');
+
+  // update user reset password token and exp
+  await UserModel.findByIdAndUpdate({ _id: user._id }, { resetToken: token, resetTokenExp: Date.now() + 600000 });
+
+  // send user password reset email
+  const emailOptions = {
+    to: userEmail,
+    from: email,
+    template: 'forgot-password',
+    subject: 'Zenva Phaser MMO Password Reset',
+    context: {
+      name: user.username,
+      url: `http://localhost:${process.env.PORT || 3000}?token=${token}`,
+    },
+  };
+  await smtpTransport.sendMail(emailOptions);
+
+  response.status(200).json({ message: 'An email has been sent to your email address. Password reset link is only valid for 10 minutes.', status: 200 });
+});
+
+router.post('/reset-password', async (request, response) => {
+  const userEmail = request.body.email;
+  const user = await UserModel.findOne({
+    resetToken: request.body.token,
+    resetTokenExp: { $gt: Date.now() },
+    email: userEmail,
+  });
+
+  if (!user) {
+    response.status(400).json({ message: 'invalid token', status: 400 });
+    return;
+  }
+
+  // ensure password was provided, and that the password matches the verified password
+  if (!request.body.password || !request.body.verifiedPassword || request.body.password !== request.body.verifiedPassword) {
+    response.status(400).json({ message: 'passwords do not match', status: 400 });
+    return;
+  }
+
+  // update user model
+  user.password = request.body.password;
+  user.resetToken = undefined;
+  user.resetTokenExp = undefined;
+  await user.save();
+
+  // send user password update email
+  const emailOptions = {
+    to: userEmail,
+    from: email,
+    template: 'reset-password',
+    subject: 'Zenva Phaser MMO Password Reset Confirmation',
+    context: {
+      name: user.username,
+    },
+  };
+  await smtpTransport.sendMail(emailOptions);
+
+  response.status(200).json({ message: 'password updated', status: 200 });
+});
+
+module.exports = router;
